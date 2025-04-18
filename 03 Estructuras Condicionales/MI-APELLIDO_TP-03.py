a5c64707ec9b6cc5a6ad6bc6b29aa2f6b186f827
GonzaloTozzi <109533353+GonzaloTozzi@users.noreply.github.com>
2025-04-18 13:17:06 -0300

    Falta el ultimo punto

--- 03 Estructuras Condicionales/MI-APELLIDO_TP-03.py
+++ 03 Estructuras Condicionales/MI-APELLIDO_TP-03.py
@@ -95,3 +95,86 @@ from statistics import mode, median, mean
 # pantalla; en caso contrario, dejar el string tal cual lo ingresó el usuario e imprimirlo por
 # pantalla.
 
+# frase = input("Por favor ingrese una frase: ")
+
+# if frase.lower()[-1] in "aeiou":
+#     frase = frase + "!"
+#     print(frase)
+
+# else:
+#     print(frase)
+
+
+
+# Escribir un programa que solicite al usuario que ingrese su nombre y el número 1, 2 o 3
+# dependiendo de la opción que desee:
+# 1. Si quiere su nombre en mayúsculas. Por ejemplo: PEDRO.
+# 2. Si quiere su nombre en minúsculas. Por ejemplo: pedro.
+# 3. Si quiere su nombre con la primera letra mayúscula. Por ejemplo: Pedro.
+# El programa debe transformar el nombre ingresado de acuerdo a la opción seleccionada por el
+# usuario e imprimir el resultado por pantalla. Nota: investigue uso de las funciones upper(),
+# lower() y title() de Python para convertir entre mayúsculas y minúsculas.
+
+
+# nombre = input("Primero ingrese su nombre: ")
+
+# menu = input("Seleccione una opcion\n1 Si quiere su nombre en mayusculas \n2 Si quiere su nombre en minusculas\n3 Si quiere su nombre con la primer letra en mayusculas ")
+
+# if menu == "1":
+#     print(nombre.upper())
+
+# elif menu == "2":
+#     print(nombre.lower())
+
+# elif menu == "3":
+#     print(nombre.title())
+
+# else:
+#     print("Opcion invalida")
+
+# Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la
+# magnitud en una de las siguientes categorías según la escala de Richter e imprima el resultado
+# por pantalla:
+# ● Menor que 3: "Muy leve" (imperceptible).
+# ● Mayor o igual que 3 y menor que 4: "Leve" (ligeramente perceptible).
+# ● Mayor o igual que 4 y menor que 5: "Moderado" (sentido por personas, pero
+# generalmente no causa daños).
+# ● Mayor o igual que 5 y menor que 6: "Fuerte" (puede causar daños en estructuras
+# débiles).
+# ● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
+# ● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).
+
+# nivel = int(input("Ingrese la magnitud del terremoto (en numeros): "))
+
+# if nivel < 3:
+#     print('"Muy leve" (imperceptible)')
+
+# elif nivel >= 3 and nivel < 4:
+#     print('"Leve (ligeramente perceptible)')
+
+# elif nivel >= 4 and nivel < 5:
+#     print('"Moderado" (sentido por personas, pero generalmente no causa daños)')
+
+# elif nivel >= 5 and nivel < 6:
+#     print('"Fuerte" (puede causar daños en estructuras debiles)')
+
+# elif nivel >= 6 and nivel < 7:
+#     print('"Muy fuerte" (puede causar daños significativos)')
+
+# elif nivel >= 7:
+#     print('"Extremo" (puede causar graves daños a gran escala)')
+
+# else:
+#     print("Ingrese un valor valido")
+
+
+
+# Escribir un programa que pregunte al usuario en cuál hemisferio se encuentra (N/S), qué mes
+# del año es y qué día es. El programa deberá utilizar esa información para imprimir por pantalla
+# si el usuario se encuentra en otoño, invierno, primavera o verano.
+
+hemis = input("En que hemisferio se encuentra? (N/S): ")
+mes = input("Que mes es?: ")
+dia = int(input("Que dia es?: "))
+
+if 
